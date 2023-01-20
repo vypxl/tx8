39f2e1507596f75f6e11e0ff362dc8376aaf23d4
vypxl <thomas@vypxl.io>
2023-01-20 11:12:31 +0100

    tests: more smaller registers test

--- test/small_registers_test.cpp
+++ test/small_registers_test.cpp
@@ -20,8 +20,13 @@ ld as -1
 lws a as
 sys &test_ai ; -1
 
+ld ab -1
+lds as ab
+lds a as
+sys &test_ai ; -1
+
 )EOF";
-    run_and_compare_num(s, {1, -1, 1, -1});
+    run_and_compare_num(s, {1, -1, 1, -1, -1});
 }
 
 TEST_F(SmallRegisters, comparisons) {
@@ -231,5 +236,409 @@ sys &test_r ; 0xffffffea
 )EOF";
     run_and_compare_num(s, {0x0u, 0x55u, 0x36u, 0x46u, 0xfffffdfcu, 0xffffffea});
 }
+
+TEST_F(SmallRegisters, mod) {
+    std::string s = R"EOF(
+lda 0x55
+ldb 0x1234
+mod ab bs
+sys &test_au ; 0x55
+
+lda 0x1234
+ldb 0x55
+mod as bb
+sys &test_au ; 0x46
+
+lda -11890
+ldb 23
+mod as bb
+sys &test_au ; 0xffffffea
+
+)EOF";
+    run_and_compare_num(s, {0x55u, 0x46u, 0xffffffeau});
+}
+
+TEST_F(SmallRegisters, min) {
+    std::string s = R"EOF(
+lda 0x55
+ldb 0x1234
+min ab bs
+sys &test_au ; 0x55
+sys &test_r ; 0x1234
+
+min as bb
+sys &test_au ; 0x34
+sys &test_r ; 0x55
+
+lda -11890
+ldb 23
+min as bb
+sys &test_ai ; -11890
+sys &test_ri ; 23
+
+min ab bs
+lds a ab ; needed because otherwise a would be -11890 again as the upper bits are not overwritten
+sys &test_ai ; -114
+sys &test_ri ; 23
+)EOF";
+    run_and_compare_num(s, {0x55u, 0x1234u, 0x34u, 0x55u, -11890, 23, -114, 23});
+}
+
+TEST_F(SmallRegisters, max) {
+    std::string s = R"EOF(
+lda 0x55
+ldb 0x1234
+max ab bs
+sys &test_au ; 0x34 ; 0x12 prefix is truncated
+sys &test_r ; 0x55
+
+lda 0x55
+ldb 0x1234
+max as bb
+sys &test_au ; 0x55
+sys &test_r ; 0x34
+
+lda -11890
+ldb 23
+max as bb
+lds a as ; needed because otherwise a would be weird as the upper bits are not overwritten
+sys &test_ai ; 23
+sys &test_ri ; -11890
+
+lda -11890
+ldb 23
+max ab bs
+lds a ab
+sys &test_ai ; 23
+sys &test_ri ; -114
+)EOF";
+    run_and_compare_num(s, {0x34u, 0x55u, 0x55u, 0x34u, 23, -11890, 23, -114});
+}
+
+TEST_F(SmallRegisters, abs) {
+    std::string s = R"EOF(
+lda -3
+abs as
+lds a as
+sys &test_ai ; 3
+sys &test_ri ; -1
+
+abs as
+sys &test_ai ; 3
+sys &test_ri ; 1
+
+lda -3
+abs ab
+lds a ab
+sys &test_ai ; 3
+sys &test_ri ; -1
+
+abs ab
+sys &test_ai ; 3
+sys &test_ri ; 1
+
+lda 0
+abs as
+sys &test_ai ; 0
+sys &test_ri ; 0
+
+abs ab
+sys &test_ai ; 0
+sys &test_ri ; 0
+)EOF";
+    run_and_compare_num(s, {3, -1, 3, 1, 3, -1, 3, 1, 0, 0, 0, 0});
+}
+
+TEST_F(SmallRegisters, sign) {
+    std::string s = R"EOF(
+lda -3
+sign as
+lds a as
+sys &test_ai ; -1
+
+sign ab
+lds a ab
+sys &test_ai ; -1
+
+lda 3
+sign as
+sys &test_ai ; 1
+
+sign ab
+sys &test_ai ; 1
+
+lda 0
+sign as
+sys &test_ai ; 0
+
+sign ab
+sys &test_ai ; 0
+)EOF";
+    run_and_compare_num(s, {-1, -1, 1, 1, 0, 0});
+}
+
+TEST_F(SmallRegisters, and) {
+    std::string s = R"EOF(
+lda 0x55
+ldb 0x1234
+and ab bs
+sys &test_au ; 0x14
+
+lda 0x55
+ldb 0x1234
+and as bb
+sys &test_au ; 0x14
+
+lda 0xfebc
+ldb 0x1234
+and ab bs
+sys &test_au ; 0xfe34
+
+lda 0xfebc
+ldb 0x1234
+and as bb
+sys &test_au ; 0x34
+)EOF";
+    run_and_compare_num(s, {0x14u, 0x14u, 0xfe34u, 0x34u});
+}
+
+TEST_F(SmallRegisters, or) {
+    std::string s = R"EOF(
+lda 0x55
+ldb 0x1234
+or ab bs
+sys &test_au ; 0x75
+
+lda 0x55
+ldb 0x1234
+or as bb
+sys &test_au ; 0x75
+
+lda 0xfebc
+ldb 0x1234
+or ab bs
+sys &test_au ; 0xfebc
+
+lda 0xfebc
+ldb 0x1234
+or as bb
+sys &test_au ; 0xfebc
+)EOF";
+    run_and_compare_num(s, {0x75u, 0x75u, 0xfebcu, 0xfebcu});
+}
+
+TEST_F(SmallRegisters, not ) {
+    std::string s = R"EOF(
+lda 0x123456
+not as
+sys &test_au ; 0x12cba9
+
+not ab
+sys &test_au ; 0x12cb56
+)EOF";
+    run_and_compare_num(s, {0x12cba9u, 0x12cb56u});
+}
+
+TEST_F(SmallRegisters, nand) {
+    std::string s = R"EOF(
+lda 0x123456
+ldb 0xfebc
+nand ab bs
+sys &test_au ; 0x1234eb
+
+lda 0x123456
+nand as bb
+sys &test_au ; 0x12ffeb
+)EOF";
+    run_and_compare_num(s, {0x1234ebu, 0x12ffebu});
+}
+
+TEST_F(SmallRegisters, xor) {
+    std::string s = R"EOF(
+lda 0x123456
+ldb 0xfebc
+xor ab bs
+sys &test_au ; 0x1234ea
+
+lda 0x123456
+xor as bb
+sys &test_au ; 0x12caea
+)EOF";
+    run_and_compare_num(s, {0x1234eau, 0x1234eau});
+}
+
+TEST_F(SmallRegisters, slr) {
+    std::string s = R"EOF(
+lda 0x123456
+ldb 0x1234de04
+slr as bs
+sys &test_au ; 0x120345
+sys &test_r ; 0x6
+
+slr ab bb
+sys &test_au ; 0x120304
+sys &test_r ; 0x5
+)EOF";
+    run_and_compare_num(s, {0x120345u, 0x6u, 0x120304u, 0x5u});
+}
+
+TEST_F(SmallRegisters, sll) {
+    std::string s = R"EOF(
+lda 0x123456
+ldb 0x1234de04
+sll as bs
+sys &test_au ; 0x124560
+sys &test_r ; 0x3
+
+sll ab bb
+sys &test_au ; 0x124500
+sys &test_r ; 0x6
+)EOF";
+    run_and_compare_num(s, {0x124560u, 0x3u, 0x124500u, 0x6u});
+}
+
+TEST_F(SmallRegisters, sar) {
+    std::string s = R"EOF(
+lda 0x123456
+ldb 0x1234de04
+sar as bs
+sys &test_au ; 0x120345
+sys &test_r ; 0x6
+
+sar ab bb
+sys &test_au ; 0x120304
+sys &test_r ; 0x5
+
+lda 0x128856
+ldb 0x1234de04
+sar as bs
+sys &test_au ; 0x12f885
+sys &test_r ; 0x6
+
+sar ab bb
+sys &test_au ; 0x12f8f8
+sys &test_r ; 0x5
+)EOF";
+    run_and_compare_num(s, {0x120345u, 0x6u, 0x120304u, 0x5u, 0x12f885u, 0x6u, 0x12f8f8u, 0x5u});
+}
+
+TEST_F(SmallRegisters, ror) {
+    std::string s = R"EOF(
+lda 0x123456
+ldb 0x1234de04
+ror as bs
+sys &test_au ; 0x126345
+
+ror ab bb
+sys &test_au ; 0x126354
+)EOF";
+    run_and_compare_num(s, {0x126345u, 0x126354u});
+}
+
+TEST_F(SmallRegisters, rol) {
+    std::string s = R"EOF(
+lda 0x123456
+ldb 0x1234de04
+rol as bs
+sys &test_au ; 0x124563
+
+rol ab bb
+sys &test_au ; 0x124536
+)EOF";
+    run_and_compare_num(s, {0x124563u, 0x124536u});
+}
+
+TEST_F(SmallRegisters, set) {
+    std::string s = R"EOF(
+lda 0
+ldb 4
+set as bs
+sys &test_au ; 0x10
+sys &test_r ; 0x0
+
+ldb 0
+set ab bb
+sys &test_au ; 0x11
+sys &test_r ; 0x0
+
+set ab 23 ; truncated => 7
+sys &test_au ; 0x91
+sys &test_r ; 0x0
+
+set ab 0
+sys &test_au ; 0x91
+sys &test_r ; 0x1
+)EOF";
+    run_and_compare_num(s, {0x10u, 0x0u, 0x11u, 0x0u, 0x91u, 0x0u, 0x91u, 0x1u});
+}
+
+TEST_F(SmallRegisters, clr) {
+    std::string s = R"EOF(
+lda 0x91
+clr ab 23
+sys &test_au ; 0x11
+sys &test_r ; 0x1
+
+clr as 4
+sys &test_au ; 0x1
+sys &test_r ; 0x1
+
+clr ab 0
+sys &test_au ; 0x0
+sys &test_r ; 0x1
+
+clr as 0
+sys &test_au ; 0x0
+sys &test_r ; 0x0
+)EOF";
+    run_and_compare_num(s, {0x11u, 0x1u, 0x1u, 0x1u, 0x0u, 0x1u, 0x0u, 0x0u});
+}
+
+TEST_F(SmallRegisters, tgl) {
+    std::string s = R"EOF(
+lda 0
+ldb 4
+tgl as bs
+sys &test_au ; 0x10
+sys &test_r ; 0x0
+
+ldb 0
+tgl ab bb
+sys &test_au ; 0x11
+sys &test_r ; 0x0
+
+tgl ab 23 ; truncated => 7
+sys &test_au ; 0x91
+sys &test_r ; 0x0
+
+lda 0x91
+tgl ab 23
+sys &test_au ; 0x11
+sys &test_r ; 0x1
+
+tgl as 4
+sys &test_au ; 0x1
+sys &test_r ; 0x1
+
+tgl ab 0
+sys &test_au ; 0x0
+sys &test_r ; 0x1
+
+)EOF";
+    run_and_compare_num(s, {0x10u, 0x0u, 0x11u, 0x0u, 0x91u, 0x0u, 0x11u, 0x1u, 0x1u, 0x1u, 0x0u, 0x1u});
+}
+
+TEST_F(SmallRegisters, test) {
+    std::string s = R"EOF(
+lda 0x123457
+test ab 8
+sys &test_r ; 0x1
+
+test as 16
+sys &test_r ; 0x1
+)EOF";
+    run_and_compare_num(s, {0x1u, 0x1u});
+}
+
 
 #pragma clang diagnostic pop
