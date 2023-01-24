1282e3324723ee9cd34f95c15c05a0d56e9c1cd7
vypxl <thomas@vypxl.io>
2023-01-24 17:49:20 +0100

    lexer: define token types

--- include/tx8/asm/lexer.hpp
+++ include/tx8/asm/lexer.hpp
@@ -1,17 +1,108 @@
 #pragma once
 
+#include "tx8/core/instruction.hpp"
+
 #include <istream>
 #include <memory>
 
 namespace tx {
-    struct LexerToken { };
+    namespace lexer::token {
+        struct EndOfFile { };
+        struct EndOfLine { };
+        struct Space { };
+        struct Register {
+            tx::Register which;
+        };
+        struct RegisterAddress {
+            tx::Register which;
+        };
+        struct AbsoluteAddress {
+            tx::uint32 address;
+        };
+        struct RelativeAddress {
+            tx::uint32 address;
+        };
+        struct Integer8 {
+            tx::uint8 value;
+        };
+        struct Integer16 {
+            tx::uint16 value;
+        };
+        struct Integer32 {
+            tx::uint32 value;
+        };
+        struct Opcode0 {
+            tx::Opcode opcode;
+        };
+        struct Opcode1 {
+            tx::Opcode opcode;
+        };
+        struct Opcode2 {
+            tx::Opcode opcode;
+        };
+        struct Float {
+            tx::float32 value;
+        };
+        struct Label {
+            std::string name;
+        };
+        struct Alias {
+            std::string name;
+        };
+        struct Invalid {
+            std::string value;
+        };
+    } // namespace lexer::token
+
     class Lexer {
       public:
+        using LexerToken = std::variant<
+            tx::lexer::token::EndOfFile,
+            tx::lexer::token::EndOfLine,
+            tx::lexer::token::Space,
+            tx::lexer::token::Register,
+            tx::lexer::token::RegisterAddress,
+            tx::lexer::token::AbsoluteAddress,
+            tx::lexer::token::RelativeAddress,
+            tx::lexer::token::Integer8,
+            tx::lexer::token::Integer16,
+            tx::lexer::token::Integer32,
+            tx::lexer::token::Opcode0,
+            tx::lexer::token::Opcode1,
+            tx::lexer::token::Opcode2,
+            tx::lexer::token::Float,
+            tx::lexer::token::Label,
+            tx::lexer::token::Alias,
+            tx::lexer::token::Invalid>;
         explicit Lexer(std::unique_ptr<std::istream> input) : is(std::move(input)) {};
 
-        tx::LexerToken next_token();
+        LexerToken next_token();
 
       private:
         std::unique_ptr<std::istream> is;
     };
 } // namespace tx
+
+#define o(t) std::ostream& operator<<(std::ostream& os, const tx::lexer::token::t& token)
+
+o(EndOfFile);
+o(EndOfLine);
+o(Space);
+o(Register);
+o(RegisterAddress);
+o(AbsoluteAddress);
+o(RelativeAddress);
+o(Integer8);
+o(Integer16);
+o(Integer32);
+o(Opcode0);
+o(Opcode1);
+o(Opcode2);
+o(Float);
+o(Label);
+o(Alias);
+o(Invalid);
+
+#undef o
+
+std::ostream& operator<<(std::ostream& os, const tx::Lexer::LexerToken& token);
--- src/asm/lexer.cpp
+++ src/asm/lexer.cpp
@@ -1,3 +1,46 @@
 #include "tx8/asm/lexer.hpp"
 
-tx::LexerToken tx::Lexer::next_token() { return tx::LexerToken(); }
+tx::Lexer::LexerToken tx::Lexer::next_token() { return tx::lexer::token::EndOfFile {}; }
+
+#define o0(t, name) \
+    std::ostream& operator<<(std::ostream& os, [[maybe_unused]] const tx::lexer::token::t& _token) { \
+        os << (name); \
+        return os; \
+    }
+#define o1(t, name, x) \
+    std::ostream& operator<<(std::ostream& os, const tx::lexer::token::t& token) { \
+        os << "( " << token.x << " )"; \
+        return os; \
+    }
+#define o1t(t, name, x, table) \
+    std::ostream& operator<<(std::ostream& os, const tx::lexer::token::t& token) { \
+        os << "( " << (table)[(tx::uint32) token.x] << " )"; \
+        return os; \
+    }
+
+o0(EndOfFile, "EOF");
+o0(EndOfLine, "EOL");
+o0(Space, "Space");
+o1t(Register, "Register", which, tx::reg_names);
+o1t(RegisterAddress, "RegisterAddress", which, tx::reg_names);
+o1(AbsoluteAddress, "AbsoluteAddress", address);
+o1(RelativeAddress, "RelativeAddress", address);
+o1(Integer8, "Integer8", value);
+o1(Integer16, "Integer16", value);
+o1(Integer32, "Integer32", value);
+o1t(Opcode0, "Opcode0", opcode, tx::op_names);
+o1t(Opcode1, "Opcode1", opcode, tx::op_names);
+o1t(Opcode2, "Opcode2", opcode, tx::op_names);
+o1(Float, "Float", value);
+o1(Label, "Label", name);
+o1(Alias, "Alias", name);
+o1(Invalid, "Invalid", value);
+
+#undef o0
+#undef o1
+#undef o1t
+
+std::ostream& operator<<(std::ostream& os, const tx::Lexer::LexerToken& token) {
+    std::visit([&os](auto&& arg) { os << arg; }, token);
+    return os;
+}
